5782faed87d17de18cc85528627e1dc6d58c1847
BAHMAN <imbahmanranjbar@gmail.com>
2025-01-18 07:24:47 +0330

    Createinit

---
+++ Expense Tracker/Models/ExpenseDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Expense_Tracker.Models;
+
+public class ExpenseDbContext:DbContext 
+{
+    public ExpenseDbContext(DbContextOptions options):base(options)
+    {
+        
+    }
+
+    public DbSet<Transaction> Transactions { get; set; }
+    public DbSet<Category> Categories { get; set; }
+}
